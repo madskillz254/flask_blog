e5a0b1d193a8e9f15c790927c76ec26535678f18
gambi <gambi@localhost.localdomain>
2020-11-06 10:38:10 +0300

    created forms.py
    added register.html

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,10 @@
 from flask import Flask ,render_template , url_for
+from forms import RegistrationForm , LoginForm
+
 app = Flask(__name__)
+
+# setting a secret key
+app.config['SECRET_KEY'] = 'b100a81daa02be3375a2922dfff54c3b'
 
 posts = [
     {
@@ -25,6 +30,18 @@ def home():
 def about():
     return render_template("about.html")
 
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template("register.html" , title='register' , form = form )
+
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template("login.html" , title='login' , form = form )
 
 if __name__ == '__main__':
     app.run(debug=True)
